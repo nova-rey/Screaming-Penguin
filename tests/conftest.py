d2990f66d47e8f8c58e10056b066ed8e35390cb7
Nova - Rey <nova.rey4200@gmail.com>
2025-12-10 18:15:23 -0600

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-"""Pytest fixtures for installer tests."""
-
-import pathlib
-import sys
-
-root = pathlib.Path(__file__).resolve().parents[1]
-root_path = str(root)
-if root_path not in sys.path:
-    sys.path.append(root_path)
